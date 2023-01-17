e44f26888b7ba900b33ca95f4ccb9136b19ba277
git_LinexMugambi <linexmugambi254@gmail.com>
2023-01-17 20:39:31 +0300

    updated

--- src/TransactionList.js
+++ src/TransactionList.js
@@ -10,3 +10,29 @@ function AccountContainer() {
 
   //manage state for user serch parameters
   const [ seaarch, setSearch] = useState("");
+
+   //Featch all transaction from server
+   useEffect(() => {
+    fetch("http://localhost:3000/transactions")
+      .then((r) => r.json())
+      .then((data) => setTransactions(data)) //Setter activity for transaction after server response, this is the data in state when the app first loads
+  }, [])
+
+  //Re-renders page with updated state after creating a new transaction, use this when trying to re render with updated data
+  function updatedTransactions(newTransactions) {
+   const updatedTransactionsArray = [...transactions, newTransactions]
+   setTransactions(updatedTransactionsArray)
+  }
+
+  //render all components
+  return (
+    <div>
+      <Search search={search} setSearch={setSearch}/>
+      <AddTransactionForm newData={updatedTransactions} />
+      <TransactionsList transactions={transactions} setTransactions={setTransactions} search={search} />
+    </div>
+  );
+}
+
+
+export default AccountContainer;
